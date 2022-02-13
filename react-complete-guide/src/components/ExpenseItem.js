a2b6cc541eae3c024419434a2c25c31fd4770977
danielghirasim <danielghirasim@gmail.com>
2022-02-14 00:09:55 +0200

    working on first components

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,5 +1,17 @@
-function ExpenseItem() {
-  return <h2>This is an Expense Item!</h2>;
+import './ExpenseItem.css';
+import './ExpenseDate';
+import ExpenseDate from './ExpenseDate';
+
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date}/>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
